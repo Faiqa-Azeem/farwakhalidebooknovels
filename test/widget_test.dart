54c1b42d1ab81a11d2576318a0799001c607ab61
Faiqa-Azeem <faiqa5600@gmail.com>
2026-06-06 15:50:39 +0500

    fix: upgrade flutter_quill to 11.5.1 and fix widget test import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:farwa_khalid/main.dart';
+import 'package:farwa_khalid_ebook_novels/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
